a8b5c6f96e850adbe19c25f5c6e2e9a5325503c4
weimingzhu <302867245@qq.com>
2020-10-31 10:19:51 +0800

    index页面功能退出完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,26 @@ $(function  ( ) {
   $.ajax({
     url: '/my/userinfo',
     success: function  (info) {
-      console.log(info);
+      if (info.status === 0) {
+        // 获取昵称或用户名显示在页面中
+        $('.welcome').html(`欢迎&nbsp;&nbsp;${info.data.nickname || info.data.username}`)
+        // 获取头像或昵称或用户名
+        if (info.data.user_pic) {
+          $('.userInfo img').show().attr('src',info.data.user_pic).prev().hide()
+        } else if (info.data.nickname) {
+          $('.avatar').show().text(info.data.nickname.slice(0,1).toUpperCase()).next().hide()
+        } else {
+          $('.avatar').show().text(info.data.username.slice(0,1).toUpperCase()).next().hide()
+        }
+      }
     }
+  })
+
+  // 点击按钮实现退出功能
+  $('.exit').on('click',function  () {
+    //  删除token
+    localStorage.removeItem('token')
+    // 跳转到login页面
+    location.href = '../../login.html'
   })
 })
