b95b1f226aac513a235ef8501957539ff8639c65
weimingzhu <302867245@qq.com>
2020-11-02 15:17:55 +0800

    user-info页面功能完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,43 @@
-$(function  ( ) {
+$(function () {
   // 在登陆的时候获取头像和用户名
-  $.ajax({
-    url: '/my/userinfo',
-    success: function  (info) {
-      if (info.status === 0) {
-        // 获取昵称或用户名显示在页面中
-        $('.welcome').html(`欢迎&nbsp;&nbsp;${info.data.nickname || info.data.username}`)
-        // 获取头像或昵称或用户名
-        if (info.data.user_pic) {
-          $('.userInfo img').show().attr('src',info.data.user_pic).prev().hide()
-        } else if (info.data.nickname) {
-          $('.avatar').show().text(info.data.nickname.slice(0,1).toUpperCase()).next().hide()
-        } else {
-          $('.avatar').show().text(info.data.username.slice(0,1).toUpperCase()).next().hide()
+  getUserInfo()
+  window.getUserInfo = getUserInfo
+  function getUserInfo() {
+    $.ajax({
+      url: '/my/userinfo',
+      success: function (info) {
+        if (info.status === 0) {
+          // 获取昵称或用户名显示在页面中
+          $('.welcome').html(
+            `欢迎&nbsp;&nbsp;${info.data.nickname || info.data.username}`
+          )
+          // 获取头像或昵称或用户名
+          if (info.data.user_pic) {
+            $('.userInfo img')
+              .show()
+              .attr('src', info.data.user_pic)
+              .prev()
+              .hide()
+          } else if (info.data.nickname) {
+            $('.avatar')
+              .show()
+              .text(info.data.nickname.slice(0, 1).toUpperCase())
+              .next()
+              .hide()
+          } else {
+            $('.avatar')
+              .show()
+              .text(info.data.username.slice(0, 1).toUpperCase())
+              .next()
+              .hide()
+          }
         }
-      }
-    },
-  })
+      },
+    })
+  }
 
   // 点击按钮实现退出功能
-  $('.exit').on('click',function  () {
+  $('.exit').on('click', function () {
     //  删除token
     localStorage.removeItem('token')
     // 跳转到login页面
--- assets/js/user/user-info.js
+++ assets/js/user/user-info.js
@@ -1,15 +1,59 @@
-$(function  ( ) {
-  //获取用户的基本信息
-  $.ajax({
-    url: '/my/userinfo',
-    success: function  (info) {
-      if (info.status === 0) {
-        var form = layui.form
-        form.val("formTest",info.data)
-      }
-    }
-  })
+$(function () {
+  //获取用户的基本信息渲染表单
+  getUser()
+  function getUser ( ) {
+    $.ajax({
+      url: '/my/userinfo',
+      success: function (info) {
+        if (info.status === 0) {
+          var form = layui.form
+          form.val('formTest', info.data)
+        }
+      },
+    })
+  }
 
   // 表单校验
-  
+  var form = layui.form
+  form.verify({
+    username: function (value, item) {
+      //value：表单的值、item：表单的DOM对象
+      if (!new RegExp('^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$').test(value)) {
+        return '用户昵称不能有特殊字符'
+      }
+      if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+        return "用户昵称首尾不能出现下划线'_'"
+      }
+      if (/^\d+\d+\d$/.test(value)) {
+        return '用户昵称不能全为数字'
+      }
+    },
+  })
+
+  // 给按钮注册事件进行用户信息修改
+  $('.myForm').on('click', '.submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+      type: 'POST',
+      url: '/my/userinfo',
+      data: form.val('formTest'),
+      success: function (info) {
+        layer.msg(info.message)
+        if (info.status === 0) {
+          // 重新渲染用户信息
+          getUser()
+          // 重新渲染头像和用户名
+          parent.window.getUserInfo()
+        }
+      },
+    })
+  })
+
+  // 给重置按钮注册事件
+  $('.myForm').on('click', '.reset', function (e) { 
+    e.preventDefault()
+    //获取用户的基本信息渲染表单
+    getUser()
+  })
+
 })
